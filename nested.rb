99dfe07e0b77d0f00504f5e0b5661e9164e0e5f7
Zach Weber <weberzt@gmail.com>
2019-12-11 20:22:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,9 +79,9 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
         :yukihiro_matsumoto => {
-          :known_for => "Ruby"
+          :known_for => "Ruby",
           :languages => ["LISP", "C"]
      }
   return programmer_hash
