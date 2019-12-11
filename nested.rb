bce71891fb942072a877981e99ea964be311dab6
Zach Weber <weberzt@gmail.com>
2019-12-11 20:26:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash
      
 end
 
